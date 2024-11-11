7e5de6abf91e42c9b2064711eda3578add9bc67a
Alisson Noli <despi.663@gmail.com>
2024-11-11 20:30:38 +0000

    Refactored code to improve readability and simplify data extraction

--- itclinical_scraper/itclinical_scraper/spiders/it_affairs.py
+++ itclinical_scraper/itclinical_scraper/spiders/it_affairs.py
@@ -6,21 +6,18 @@ class ItAffairsSpider(scrapy.Spider):
     start_urls = ["https://itclinical.com/it.php"]
 
     def parse(self, response):
-        # Select all links in the "IT Affairs" section
+        # Select links in the "IT Affairs" section
         product_links = response.xpath(
             '//ul[@class="menu"]/li[2]/ul/li/a')
 
         # Reverse order of the links
-        product_links.reverse()
+        product_links = product_links[::-1]
 
         # Navigate to each product link
         for link in product_links:
-            # Extract title and URL
-            title = link.xpath('text()').get().strip()
             url = link.xpath('@href').get()
-
-            # Follow the link to parse each section, using errback to handle any potential errors
-            yield response.follow(url, self.parse_section, errback=self.handle_error, meta={'link_title': title})
+            # Follow the link to parse each section, using errback to handle potential errors
+            yield response.follow(url, self.parse_section, errback=self.handle_error)
 
     def handle_error(self, failure):
         # Log the URL and details if a request fails
@@ -28,24 +25,21 @@ class ItAffairsSpider(scrapy.Spider):
         self.logger.error(repr(failure))
 
     def parse_section(self, response):
-        # Get the title passed from the previous request
-        link_title = response.meta['link_title']
-
-        # Extract the product title from the page or fallback to the link title
-        product_title = response.xpath(
+        # Get title directly from the software page
+        title = response.xpath(
             '//div[contains(@class, "sixteen floated page-title")]//h2/text()').get()
-        title = product_title.strip() if product_title else link_title
+        title = title.strip() if title else "Unknown Title"
 
         # Collect the list of features
         features = response.xpath(
             '(//ul[contains(@class, "check-list")])[1]//li/text()').getall()
 
-        # If no features are found, log a warning and skip this item
+        # If no features, log a warning and skip this item
         if not features:
             self.logger.warning(f'No features found for {title}')
             return
 
-        # Print the title and features
+        # Print title and features
         print(f"\n{title}\n")
         for feature in features:
             print(f"  - {feature.strip()}")
